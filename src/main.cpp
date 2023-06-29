8a5a00cdac8b93d11ac07c3021d3284ebe75e053
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2023-06-29 15:11:37 +0200

    Use HW serial.

--- src/main.cpp
+++ src/main.cpp
@@ -20,31 +20,30 @@
 #define DRVENABLE PA15
 #define DRVFAULT PB4
 #define DRVRESET PB5
-BLDCMotor motor = BLDCMotor(8, 6.9 /*, 480*/);  // 8 pole pairs, 6.9, 480kV estimated (todo: bump up to +70% or measure?)
+// BLDCMotor motor = BLDCMotor(8, 6.9 /*, 480*/);  // 8 pole pairs, 6.9, 480kV estimated (todo: bump up to +70% or measure?)
+BLDCMotor motor = BLDCMotor(6);
 BLDCDriver3PWM driver = BLDCDriver3PWM(PWM1, PWM2, PWM3, DRVENABLE); //STM32G031K8Ux + MOSQUITO
-float target = 0.0;
+float target_velocity = 5.0;
 
 #define SENSOR1_CS PA4 // some digital pin that you're using as the nCS pin
-SPIClass spi1(PA7, PA6, PA5);
-MagneticSensorSC60228 sensor(SENSOR1_CS, 2048);
+//SPIClass spi1(PA7, PA6, PA5);
+//MagneticSensorSC60228 sensor(SENSOR1_CS, 2048);
 
 // #define SENSOR1_PWM PC6
 // MagneticSensorPWM sensor = MagneticSensorPWM(SENSOR1_PWM, 20, 976);  // don't use this, it's very noisy on PWM
 // void doPWM(){sensor.handlePWM();}
 
-RTTStream rtt;
-
-// HardwareSerial Serial1(serial1_rx_pin, serial1_tx_pin);
+HardwareSerial serial(serial1_rx_pin, serial1_tx_pin);
 
 void serialLoop() {
   static String rx;
-  while(rtt.available()){
-    char in = (char) rtt.read();
+  while(serial.available()){
+    char in = (char) serial.read();
     rx += in;
     if (in=='\n') {
-      rtt.println("parsing rx");
-      target = rx.toFloat();
-      rtt.print("Target = "); rtt.println(target);
+      serial.println("parsing rx");
+      target_velocity = rx.toFloat();
+      serial.print("Target = "); serial.println(target_velocity);
       rx = "";
     }
   }
@@ -55,23 +54,23 @@ void setup() {
   pinMode(DRVRESET, OUTPUT); // Reset pin
   digitalWrite(DRVRESET, HIGH); // Default low
   
-  rtt.println("Starting setup");
+  serial.begin(115200);
+  serial.println("Starting setup");
   _delay(500);
   driver.voltage_power_supply = 12;
   driver.pwm_frequency = 20000;
   driver.init();
   motor.linkDriver(&driver);
-  motor.voltage_limit = 3; // [V]
-  motor.velocity_limit = 20; // [rad/s] ~191rpm
+  motor.voltage_limit = 4; // [V]
+  motor.velocity_limit = 1000; // [rad/s] ~191rpm
   motor.torque_controller  = TorqueControlType::voltage;
 
-  motor.PID_velocity.P = 0.02;  // 0.02
-  motor.PID_velocity.I = 0.2;  // 0.1
-  motor.PID_velocity.D = 0.0001;
-  motor.PID_velocity.output_ramp = 300;
-  motor.LPF_velocity.Tf = 0.05;  // 50ms low pass filter
-  motor.controller = MotionControlType::velocity;
-  motor.voltage_sensor_align = 1; // used by closed loop
+  // motor.PID_velocity.P = 0.02;  // 0.02
+  // motor.PID_velocity.I = 0.2;  // 0.1
+  // motor.PID_velocity.D = 0.0001;
+  // motor.PID_velocity.output_ramp = 300;
+  // motor.LPF_velocity.Tf = 0.05;  // 50ms low pass filter
+  motor.controller = MotionControlType::velocity_openloop;
   motor.current_limit = 1;
 
   // for PWM sensor:
@@ -79,18 +78,18 @@ void setup() {
   // sensor.enableInterrupt(doPWM);
 
   // SPI sensor interface:
-  sensor.init(&spi1);
+  // sensor.init(&spi1);
 
-  motor.useMonitoring(rtt); 
-  motor.linkSensor(&sensor);
+  motor.useMonitoring(serial);
+  // motor.linkSensor(&sensor);
 
-  rtt.println("Setup complete");
+  serial.println("Setup complete");
 
   motor.init();
-  //motor.initFOC();  // to get the two parameters below
-  motor.initFOC(5.89, Direction::CW); 
+  motor.initFOC();  // to get the two parameters below
+  // motor.initFOC(5.89, Direction::CW); 
 
-  rtt.println("FOC Init complete");
+  serial.println("FOC Init complete");
 
   //rtt.println(motor.zero_electric_angle);
   //rtt.println(motor.sensor_direction);
@@ -101,7 +100,7 @@ void loop() {
   //motor.PID_velocity.P = target;
 
   motor.loopFOC();
-  motor.move(target);
+  motor.move(target_velocity);
 
   motor.monitor();
   serialLoop();
